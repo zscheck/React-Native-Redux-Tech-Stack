05468af9631a4867891010e50a81f2158556a6ba
Zach Scheck <zach.scheck@gmail.com>
2018-02-18 16:03:54 -0800

    animation added and cleaner layout

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+    Text, 
+    TouchableWithoutFeedback, 
+    View, 
+    LayoutAnimation
+ } from 'react-native';
 import {CardSection } from './common';
 import * as actions from '../actions';
 
 
 class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
     renderDescription() {
         const { library, expanded } = this.props;
         if (expanded) {
             return (
-                <Text>{ library.description }</Text>
+                <CardSection>
+                    <Text>{ library.description }</Text>
+                </CardSection>
             )
         }
     }
